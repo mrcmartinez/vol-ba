fdc556d6ab66c7c03373b8b53854f802993cbd9c
[mau-banco] <[mrcmartinezag@gmail.com]>
2022-02-03 10:38:18 -0600

    commitdatos-modelo

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -4,10 +4,15 @@
             parent::__construct();
         }
         public function home($parems){
-            echo "mensaje desde el controlador";
+            // echo "mensaje desde el controlador";
         }
          public function datos($parems){
              echo "Dato recibido:".$parems;
          }
+         public function datosusuarios($parems){
+            $datosusuarios= $this->model->getDatosUsuarios($parems);
+            echo $datosusuarios;
+            echo "prueba2";
+        }
     }
 ?>
